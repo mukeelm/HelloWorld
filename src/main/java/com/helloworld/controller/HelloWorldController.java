69797e8508f7e4bd492f9fa31e06d4313cc1ed61
Mohammad Mukeel <mukeelm@gmail.com>
2021-05-23 19:07:55 +0200

    Hello World Project on git hub

---
+++ src/main/java/com/helloworld/controller/HelloWorldController.java
@@ -0,0 +1,21 @@
+package com.helloworld.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@RequestMapping("/api/")
+public class HelloWorldController 
+{
+@GetMapping("helloWorld")
+public String helloWorld()
+{
+return "Hello World !!!";
+}
+
+    @GetMapping("helloSpring")
+    public String helloSpring()
+    {
+        return "Hello Spring !!!";
+    }
+}
